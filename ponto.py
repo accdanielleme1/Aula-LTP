6d63f408e107ec4c8c312710aa59663c6bc55f26
unknown <daniel.leme@aluno.ifsp.edu.br>
2025-04-03 08:53:10 -0300

    adcionando comentarios

--- ponto.py
+++ ponto.py
@@ -1,22 +1,24 @@
 class Ponto:
     """ Classe ponto com suas coordenadas"""
 
-    def __init__(self, initX, initY):
-
+    def __init__(self, initX, initY): #construtor
         self.x = initX
         self.y = initY
 
+   # pegando o valores dos pontos e colocando     
     def getX(self):
         return self.x
     
     def setX(self,x):
         self.x=x
 
+    """ Tarefa feita """
+    #Tarefa é fazer o mesmo feito no "X", Altere a classe Ponto acrescentando as funções getY e setY e uma função para calcular a distância entre dois pontos.
     def getY(self,y):
         return self.y
-
     def setY(self,y):
         self.y=y
+    #calculando distancias
     def distancia(self,q):
         return ((self.x-q.x)**2+(self.y-q.y)**2)**0.5
     
