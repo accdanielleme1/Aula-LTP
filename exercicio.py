25841bc692d688570527fd7491b83920e758620e
unknown <daniel.leme@aluno.ifsp.edu.br>
2025-04-03 09:17:15 -0300

    novos exercicios

---
+++ exercicio.py
@@ -0,0 +1,19 @@
+''' Exercicio com companhia da professora'''
+
+#classe sendo criado
+class ContaBancaria:
+    def __init__(self,cpf: str, saldo:float=0.0): #construtor
+        self.cpf = cpf
+        self.saldo = saldo
+
+    def getSaldo(self): #Pegando saldo
+        return self.saldo
+    
+    def getCPF(self): #Pegando CPF
+        return self.cpf
+    
+conta1 = ContaBancaria("123.456.267-60",1250) #primeira conta bancaria, dada por variavel, e add cpf e saldo na conta;
+ ''' mostrando cpf'''
+print('cpf da conta: ', conta1.getCPF())
+print('saldo da conta: ', conta1.getSaldo())
+
